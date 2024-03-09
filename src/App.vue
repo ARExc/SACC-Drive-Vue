2b2a8e8b0d92401b97a5a05c50d28fbef09c5511
OsmanthusPeace <3139714892@qq.com>
2024-03-09 17:04:36 +0800

    test:test the upload

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,10 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Sharding/>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
+import Sharding from './components/Sharding.vue'
 </script>
 
 <style>
