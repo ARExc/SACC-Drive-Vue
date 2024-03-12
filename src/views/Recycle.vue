497cd25435b1a9b77b28456217d66f10cada8e40
Hu Tianyv <1394139263@qq.com>
2024-03-12 11:47:58 +0800

    a tiny change

--- src/views/Recycle.vue
+++ src/views/Recycle.vue
@@ -1,14 +1,34 @@
 <template>
   <div class="container">
     <div class="main">
+
     </div>
   </div>
 </template>
 
-<script setup>
-import Side from "@/views/Side.vue";
-import Header from "@/views/Header.vue";
-import Breadcrumb from "@/views/Breadcrumb.vue";
+<script>
+import request from '@/utility/request';
+export default {
+  data(){
+    return ;
+  },
+  created(){
+  request.post('/api/recycle/getRecycleList', {
+        studentId: this.studentId,
+        password: this.password,
+        errorMessage:this.errorMessage
+      }).then(response => {
+        if(response.status>=200&&response.status<300)
+        this.$store.commit('setToken', response.data.token);
+        this.$router.push('/home');
+      }).catch(error => {
+        // this.$router.push('/error');
+        ElMessage.error('errorMessage')
+      });
+
+}
+
+}
 </script>
 
 <style scoped>
